25fa8d819090679c7217c81a965e06fb324eb4e2
sjsjsj1246 <sjsjsj1246@gmail.com>
2021-12-18 23:07:30 +0900

    Feat: auth, user, project API 작성, 리듀서 설계

--- api/auth.ts
+++ api/auth.ts
@@ -1,1 +1,33 @@
-export {};
+import axios from 'axios';
+import { User } from '../types';
+
+export const kakaoOauth = async (accessToken: string) => {
+  const { data } = await axios.post<any>(`${process.env.NEXT_PUBLIC_API_URL}/oauth/kakao`, {
+    accessToken,
+  });
+  return data;
+};
+
+export const googleOauth = async (accessToken: string) => {
+  const { data } = await axios.post<any>(`${process.env.NEXT_PUBLIC_API_URL}/oauth/google`, {
+    accessToken,
+  });
+  return data;
+};
+
+export const signUp = async (user: Partial<User>) => {
+  const { data } = await axios.post<User>(`${process.env.NEXT_PUBLIC_API_URL}/auth`, {
+    user,
+  });
+  return data;
+};
+
+export const signUser = async (id: User['id']) => {
+  const { data } = await axios.post<any>(`${process.env.NEXT_PUBLIC_API_URL}/auth/${id}`);
+  return data;
+};
+
+export const dropUser = async (id: User['id']) => {
+  const { data } = await axios.delete<any>(`${process.env.NEXT_PUBLIC_API_URL}/auth/${id}`);
+  return data;
+};
