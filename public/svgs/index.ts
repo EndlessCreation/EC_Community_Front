d58cbae6a302cb1f306a79e936f9da3a0297f2ad
sjsjsj1246 <sjsjsj1246@gmail.com>
2021-12-18 21:46:30 +0900

    Feat: 로그인 화면 개발

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,3 +1,9 @@
+export { default as LogoWithText } from './LogoWithText.svg';
+
 export { default as SharingIcon } from './SharingIcon.svg';
 export { default as CooperationIcon } from './CooperationIcon.svg';
 export { default as GrowthIcon } from './GrowthIcon.svg';
+
+//auth
+export { default as KakaoIcon } from './KakaoIcon.svg';
+export { default as GoogleIcon } from './GoogleIcon.svg';
