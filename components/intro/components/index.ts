b6dc835d265eb63787cc8121fd52cee2a58fcbf8
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-09-06 17:23:36 +0900

    feat: community, admin 관련 화면 제거 #62

--- components/intro/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as PlayTitleAction } from './PlayTitleAction';
-export { default as PlayOpacity } from './PlayOpacity';
-export { default as PlayScale } from './PlayScale';
-export { default as PlayBackground } from './PlayBackground';
-export { default as PlayFloatAction } from './PlayFloatAction';
-export { default as PlayBlur } from './PlayBlur';
-export { default as PlayContactUs } from './PlayContactUs';
