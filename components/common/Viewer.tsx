bfe12221c3cbab619915453e45af268845d0894d
sjsjsj1246 <sjsjsj1246@gmail.com>
2023-04-09 16:08:17 +0900

    Fix: type error

--- components/common/Viewer.tsx
+++ components/common/Viewer.tsx
@@ -3,6 +3,7 @@ import { Viewer as TuiViewer, ViewerProps } from '@toast-ui/react-editor';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import 'prismjs/themes/prism.css';
 import Prism from 'prismjs';
+// @ts-ignore
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
 
 export interface TuiViewerWithForwardedProps extends ViewerProps {
